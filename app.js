028f806c3794df2549549f282f587f98fe00233e
Sergey Zelinsky <zelinsky.serega@gmail.com>
2020-08-07 20:57:26 +0300

    optimized code and removed dublicates

--- app.js
+++ app.js
@@ -1,42 +1,18 @@
 import { words } from './data.js';
 
-const jumbotron = document.querySelector('.jumbotron');
-const btn25 = document.getElementById('w25');
-const btn50 = document.getElementById('w50');
-const btn75 = document.getElementById('w75');
-const btn100 = document.getElementById('w100');
+//const jumbotron = document.querySelector('.jumbotron');
 const listItems = document.getElementById('words');
 
-btn25.addEventListener('click', {
-  handleEvent: mixArray,
-  arr: words,
-  qty: 25,
-  list: listItems,
-});
-btn50.addEventListener('click', {
-  handleEvent: mixArray,
-  arr: words,
-  qty: 50,
-  list: listItems,
-});
-btn75.addEventListener('click', {
-  handleEvent: mixArray,
-  arr: words,
-  qty: 75,
-  list: listItems,
-});
-btn100.addEventListener('click', {
-  handleEvent: mixArray,
-  arr: words,
-  qty: 100,
-  list: listItems,
-});
-
-//jumbotron.remove()
-
-// function someFunk() {
-//   console.log(this.a, this.b);
-// }
+(function (...args) {
+  for (let arg of args) {
+    document.getElementById('btn' + arg).addEventListener('click', {
+      handleEvent: mixArray,
+      arr: words,
+      qty: arg,
+      list: listItems,
+    });
+  }
+})(25, 50, 75, 100);
 
 function mixArray() {
   const result = [];
@@ -44,11 +20,7 @@ function mixArray() {
     let randomIndex = Math.floor(Math.random() * this.arr.length);
     if (result.includes(this.arr[randomIndex])) continue;
     result[result.length] = this.arr[randomIndex];
-    //!result.includes(arr[randomIndex]) ?
-    //result[result.length] = arr[randomIndex] : null
   }
-  //return result;
-  //console.log(result)
   showResult(result, this.list);
 }
 
@@ -68,5 +40,3 @@ function showResult(arr, list) {
   );
   list.insertAdjacentHTML('beforeend', listString);
 }
-
-// console.log(mixArray(words, 20));
